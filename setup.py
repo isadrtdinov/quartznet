503cfa19eab2bd60eeaf89c8151af311bc557a71
Ubuntu <isadrtdinov@isadrtdinovDL.11p2tmnjmz3ubowvwanmq5kfua.bx.internal.cloudapp.net>
2020-10-16 08:44:35 +0000

    move requirements to dockerfile

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import find_packages, setup
 
 
-def get_requirements():
-    with open("requirements.txt", "r") as f:
-        requirements = f.read().splitlines()
-
-    return requirements
-
-
 setup(
     name="asr",
     version="0.0.1",
     author="isadrtdinov",
     package_dir={"": "asr"},
     packages=find_packages("asr"),
-    install_requires=get_requirements(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
